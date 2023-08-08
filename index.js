3a0c43503b891dfcd57495839624d5eeae5ca8b1
Rikelmy-Lopes <rikelmylopes899@gmail.com>
2023-08-08 20:02:47 -0300

    feat: adicionado metodo sincrono

--- index.js
+++ index.js
@@ -1,48 +1,52 @@
-const fs = require('fs/promises');
+const fs = require('fs');
 const { join } = require('path');
+const getSize = require('./promises/index.js')
 
-const readDirectory = async (path, arrayOfFiles = []) => {
-  try {
-    const files = await fs.readdir(path, { withFileTypes: true })
-
-    await Promise.all(files.map(async (file) => {
-      if (file.isDirectory()) {
-        await readDirectory(join(path, file.name), arrayOfFiles);
-      } else {
-        arrayOfFiles.push(join(path, file.name));
-      }
-    }));
-  } catch (error) {
-    console.log(error.message)
-  }
-  
-  return arrayOfFiles;
-};
-
-const getSize = async (arrayOfFiles) => {
-  let size = 0;
-
-  await Promise.all(arrayOfFiles.map(async (file) => {
+const readDirectory = (path, arrayOfFiles = []) => {
     try {
-      const fileStats = await fs.stat(file);
-      size += fileStats.size;
+      const files = fs.readdirSync(path, { withFileTypes: true })
+  
+       files.map((file) => {
+        if (file.isDirectory()) {
+           readDirectory(join(path, file.name), arrayOfFiles);
+        } else {
+          arrayOfFiles.push(join(path, file.name));
+        }
+      });
     } catch (error) {
       console.log(error.message)
     }
-  }));
-
-  return size;
-};
-
-/**
- * Calculates the total size of a folder and its subfolders.
- * @param {string} path - The path to the folder.
- * @returns {Promise<number>} - The total size in bytes.
- * @since v1.0.0
- */
-const main = async (path) => {
-  const arrayOfFiles = await readDirectory(path);
-  return await getSize(arrayOfFiles);
-}
-
-module.exports = main;
+    
+    return arrayOfFiles;
+  };
+  
+  const calculateSize = (arrayOfFiles) => {
+    let size = 0;
+  
+     Promise.all(arrayOfFiles.map( (file) => {
+      try {
+        const fileStats = fs.statSync(file);
+        size += fileStats.size;
+      } catch (error) {
+        console.log(error.message)
+      }
+    }));
+  
+    return size;
+  };
+  
+  /**
+   * Calculates the total size of a folder and its subfolders.
+   * @param {string} path - The path to the folder.
+   * @returns {number} - The total size in bytes.
+   * @since v1.0.0
+   */
+  const getSizeSync = (path) => {
+    const arrayOfFiles = readDirectory(path);
+    return calculateSize(arrayOfFiles);
+  }
+  
+  module.exports = {
+    getSize,
+    getSizeSync
+  }
---
+++ promises/index.js
@@ -0,0 +1,48 @@
+const fs = require('fs/promises');
+const { join } = require('path');
+
+const readDirectory = async (path, arrayOfFiles = []) => {
+  try {
+    const files = await fs.readdir(path, { withFileTypes: true })
+
+    await Promise.all(files.map(async (file) => {
+      if (file.isDirectory()) {
+        await readDirectory(join(path, file.name), arrayOfFiles);
+      } else {
+        arrayOfFiles.push(join(path, file.name));
+      }
+    }));
+  } catch (error) {
+    console.log(error.message)
+  }
+  
+  return arrayOfFiles;
+};
+
+const calculateSize = async (arrayOfFiles) => {
+  let size = 0;
+
+  await Promise.all(arrayOfFiles.map(async (file) => {
+    try {
+      const fileStats = await fs.stat(file);
+      size += fileStats.size;
+    } catch (error) {
+      console.log(error.message)
+    }
+  }));
+
+  return size;
+};
+
+/**
+ * Calculates the total size of a folder and its subfolders.
+ * @param {string} path - The path to the folder.
+ * @returns {Promise<number>} - The total size in bytes.
+ * @since v1.0.0
+ */
+const getSize = async (path) => {
+  const arrayOfFiles = await readDirectory(path);
+  return await calculateSize(arrayOfFiles);
+}
+
+module.exports = getSize;
