ed53bb22b62ebb85be19127e6b52386f72b0e6be
Rikelmy Lopes <rikelmylopes899@gmail.com>
2023-08-08 12:51:40 -0300

    feat: get folder size in bytes

---
+++ index.js
@@ -0,0 +1,44 @@
+const fs = require('fs/promises');
+const { join } = require('path');
+
+const readDirectory = async (path, arrayOfFiles = []) => {
+  const files = await fs.readdir(path, { withFileTypes: true });
+
+  await Promise.all(files.map(async (file) => {
+    if (file.isDirectory()) {
+      await readDirectory(join(path, file.name), arrayOfFiles);
+    } else {
+      arrayOfFiles.push(join(path, file.name));
+    }
+  }));
+  
+  return arrayOfFiles;
+};
+
+const getSize = async (arrayOfFiles) => {
+  let size = 0;
+
+  await Promise.all(arrayOfFiles.map(async (file) => {
+    try {
+      const fileStats = await fs.stat(file);
+      size += fileStats.size;
+    } catch (error) {
+      return;
+    }
+  }));
+
+  return size;
+};
+
+/**
+ * Calculates the total size of a folder and its subfolders.
+ * @param {string} path - The path to the folder.
+ * @returns {Promise<number>} - The total size in bytes.
+ * @since v1.0.0
+ */
+async function main(path) {
+  const arrayOfFiles = await readDir(path);
+  return await getSize(arrayOfFiles);
+}
+
+module.exports = main;
