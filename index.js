119520bac6ee4d2370164a0311c86d234f113ecc
Rikelmy Lopes <rikelmylopes899@gmail.com>
2023-08-10 23:14:47 -0300

    feat: adicionado callback para lidar com erros

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const fs = require('fs');
 const { join } = require('path');
 const getSize = require('./promises/index.js');
 
-const getFileSize = (filePath) => {
+const getFileSize = (filePath, callback) => {
   try {
     const fileStats = fs.statSync(filePath);
     return fileStats.size;
   } catch (error) {
-    console.log(error.message);
+    typeof callback === 'function' && callback(error);
     return 0;
   }
 };
@@ -16,7 +16,7 @@ const getEntries = (path) => {
   return fs.readdirSync(path, { withFileTypes: true });
 };
 
-const calculateSize = (path) => {
+const calculateSize = (path, callback) => {
   let totalSize = 0;
   try {
     const entries = getEntries(path);
@@ -24,31 +24,36 @@ const calculateSize = (path) => {
 
     for (let i = 0; i < len; i += 1) {
       const entryPath = join(path, entries[i].name);
-      totalSize += entries[i].isDirectory() ? calculateSize(entryPath) : getFileSize(entryPath);
+      totalSize += entries[i].isDirectory() ? calculateSize(entryPath, callback) : getFileSize(entryPath, callback);
     }
 
   } catch (error) {
-    console.log(error.message);
+    typeof callback === 'function' && callback(error);
     return 0;
   }
     
   return totalSize;
+  
 };
+
+
+
 
   
 /**
    * Calculates the total size of a folder and its subfolders.
    * @param {string} path - The path to the folder.
+   * @param {function(Error): void=} callback - A callback function that handles potential errors during the folder size calculation.
    * @returns {number} - The total size in bytes.
    * @throws {TypeError} - Throws an error if the provided path is not a string.
    * @since v1.0.0
    */
-const getSizeSync = (path) => {
+const getSizeSync = (path, callback) => {
   if (typeof path !== 'string') {
     throw new TypeError(`Path must be a string. Received: ${typeof path}`);
   }
 
-  return calculateSize(path);
+  return calculateSize(path, callback);
 };
   
 module.exports = {
--- promises/index.js
+++ promises/index.js
@@ -1,12 +1,12 @@
 const fs = require('fs/promises');
 const { join } = require('path');
 
-const getFileSize = async (filePath) => {
+const getFileSize = async (filePath, callback) => {
   try {
     const fileStats = await fs.stat(filePath);
     return fileStats.size;
   } catch (error) {
-    console.log(error.message);
+    typeof callback === 'function' && callback(error);
     return 0;
   }
 };
@@ -15,21 +15,22 @@ const getEntries = async (path) => {
   return await fs.readdir(path, { withFileTypes: true });
 };
 
-const calculateSize = async (path) => {
+const calculateSize = async (path, callback) => {
   let totalSize = 0;
   try {
     const entries = await getEntries(path);
 
     const sizes = await Promise.all(entries.map(async (entry) => {
       const entryPath = join(path, entry.name);
-      return entry.isDirectory() ? await calculateSize(entryPath) : await getFileSize(entryPath);
+      return entry.isDirectory() ? await calculateSize(entryPath, callback) : await getFileSize(entryPath, callback);
     }));
 
     totalSize = sizes.reduce((acc, size) => acc + size, 0);
 
 
   } catch (error) {
-    console.log(error.message);
+    typeof callback === 'function' && callback(error);
+    return 0;
   }
 
   return totalSize;
@@ -40,16 +41,17 @@ const calculateSize = async (path) => {
 /**
    * Calculates the total size of a folder and its subfolders.
    * @param {string} path - The path to the folder.
+   * @param {function(Error): void=} callback - A callback function that handles potential errors during the folder size calculation.
    * @returns {Promise<number>} - The total size in bytes.
    * @throws {TypeError} - Throws an error if the provided path is not a string.
    * @since v1.0.0
    */
-const getSize = async (path) => {
+const getSize = async (path, callback) => {
   if (typeof path !== 'string') {
     throw new TypeError(`Path must be a string. Received: ${typeof path}`);
   }
 
-  return await calculateSize(path);
+  return await calculateSize(path, callback);
 };
 
 module.exports = getSize;
