bf95aeaeb7e9f2ea89e945ca60403577c4b7c323
Rikelmy Lopes <rikelmylopes899@gmail.com>
2023-08-14 13:57:26 -0300

    feat: adicionando typescript

--- src/index.ts
+++ src/index.ts
@@ -2,60 +2,42 @@ import { statSync, readdirSync, Dirent } from 'fs';
 import { join } from 'path';
 import getDirSize from './promises/index';
 
-const getFileSize = (filePath: string, callback?: (err: unknown) => void): number => {
+
+const getFileSize = (filePath: string, callback?: (err: Error) => void): number => {
   try {
     const fileStats = statSync(filePath);
     return fileStats.size;
   } catch (error) {
-    typeof callback === 'function' && callback(error);
+    typeof callback === 'function' && callback(error as Error);
     return 0;
   }
 };
 
-const getDirEntries = (dirPath: string, callback?: (err: unknown) => void): Dirent[] => {
+const getDirEntries = (dirPath: string, callback?: (err: Error) => void): Dirent[] => {
   try {
     return readdirSync(dirPath, { withFileTypes: true });
   } catch (error) {
-    typeof callback === 'function' && callback(error);
+    typeof callback === 'function' && callback(error as Error);
     return [];
   }
 };
 
-const calculateTotalDirSize = (dirPath: string, callback?: (err: unknown) => void): number => {
+const calculateTotalDirSize = (dirPath: string, callback?: (err: Error) => void): number => {
   let totalSize = 0;
   const entries = getDirEntries(dirPath, callback);
   const len = entries.length;
 
   for (let i = 0; i < len; i += 1) {
     const entryPath = join(dirPath, entries[i].name);
-    totalSize += entries[i].isDirectory() ? calculateTotalDirSize(entryPath, callback) : getFileSize(entryPath, callback);
+    totalSize += entries[i].isDirectory() ? calculateTotalDirSize(entryPath, callback) : 
+      getFileSize(entryPath, callback);
   }
     
   return totalSize;
 };
 
 
-const getDirSizeSync = (dirPath: string, callback?: (err: unknown) => void): number => {
-  if (typeof dirPath !== 'string') {
-    throw new TypeError(`Path must be a string. Received: ${typeof dirPath}`);
-  }
-
-  return calculateTotalDirSize(dirPath, callback);
-};
-  
-export {
-
-  /**
-   * Calculates the total size of a folder and its subfolders.
-   * @param {string} dirPath - The path to the folder.
-   * @param {function} callback - A callback function that handles potential errors during the folder size calculation.
-   * @returns {Promise<number>} - The total size in bytes.
-   * @throws {TypeError} - Throws an error if the provided path is not a string.
-   * @since v1.0.0
-   */
-  getDirSize,
-
-  /**
+/**
    * Calculates the total size of a folder and its subfolders.
    * @param {string} dirPath - The path to the folder.
    * @param {function} callback - A callback function that handles potential errors during the folder size calculation.
@@ -63,5 +45,15 @@ export {
    * @throws {TypeError} - Throws an error if the provided path is not a string.
    * @since v1.0.0
    */
+const getDirSizeSync = (dirPath: string, callback?: (err: Error) => void): number => {
+  if (typeof dirPath !== 'string') {
+    throw new TypeError(`Path must be a string. Received: ${typeof dirPath}`);
+  }
+
+  return calculateTotalDirSize(dirPath, callback);
+};
+
+export {
+  getDirSize,
   getDirSizeSync
 };
--- src/promises/index.ts
+++ src/promises/index.ts
@@ -2,27 +2,27 @@ import { Dirent } from 'fs';
 import { stat, readdir } from 'fs/promises';
 import { join } from 'path';
 
-const getFileSize = async (filePath: string, callback?: (err: unknown) => void): Promise<number> => {
+const getFileSize = async (filePath: string, callback?: (err: Error) => void): Promise<number> => {
   try {
     const fileStats = await stat(filePath);
     return fileStats.size;
   } catch (error) {
-    typeof callback === 'function' && callback(error);
+    typeof callback === 'function' && callback(error as Error);
     return 0;
   }
 };
 
-const getDirEntries = async (dirPath: string, callback?: (err: unknown) => void): Promise<Dirent[]> => {
+const getDirEntries = async (dirPath: string, callback?: (err: Error) => void): Promise<Dirent[]> => {
   try {
     const entries = await readdir(dirPath, { withFileTypes: true });
     return entries;
   } catch (error) {
-    typeof callback === 'function' && callback(error);
+    typeof callback === 'function' && callback(error as Error);
     return [];
   }
 };
 
-const calculateTotalDirSize = async (dirPath: string, callback?: (err: unknown) => void): Promise<number> => {
+const calculateTotalDirSize = async (dirPath: string, callback?: (err: Error) => void): Promise<number> => {
   let totalSize = 0;
 
   const entries = await getDirEntries(dirPath, callback);
@@ -37,7 +37,16 @@ const calculateTotalDirSize = async (dirPath: string, callback?: (err: unknown) 
   return totalSize;
 };
 
-const getDirSize = async (dirPath: string, callback?: (err: unknown) => void): Promise<number> => {
+
+/**
+   * Calculates the total size of a folder and its subfolders.
+   * @param {string} dirPath - The path to the folder.
+   * @param {function} callback - A callback function that handles potential errors during the folder size calculation.
+   * @returns {Promise<number>} - The total size in bytes.
+   * @throws {TypeError} - Throws an error if the provided path is not a string.
+   * @since v1.0.0
+   */
+const getDirSize = async (dirPath: string, callback?: (err: Error) => void): Promise<number> => {
   if (typeof dirPath !== 'string') {
     throw new TypeError(`Path must be a string. Received: ${typeof dirPath}`);
   }
